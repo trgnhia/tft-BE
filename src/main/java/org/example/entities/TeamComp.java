02bb08e93c7080c8131b0b4952cd2ca4d73d57b2
Hoanglv <hoanglv1106@gmail.com>
2026-04-01 17:02:34 +0700

    fix: update entities

--- src/main/java/org/example/entities/TeamComp.java
+++ src/main/java/org/example/entities/TeamComp.java
@@ -1,4 +1,26 @@
 package org.example.entities;
 
-public class TeamComp {
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.example.common.entity.AuditableEntity;
+
+@Entity
+@Table(name = "team_comp")
+@Getter
+@Setter
+public class TeamComp extends AuditableEntity {
+
+    @Column(name = "set_id")
+    private Long setId;
+
+    @Column(name = "slug", unique = true)
+    private String slug;
+
+    @Column(name = "style")
+    private String style;
+
+    @Column(name = "name")
+    private String name;
+
 }
--- src/main/java/org/example/entities/TeamCompChamp.java
+++ src/main/java/org/example/entities/TeamCompChamp.java
@@ -1,4 +1,33 @@
 package org.example.entities;
 
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@Entity
+@Table(name = "team_comp_champ")
+@Getter
+@Setter
+@IdClass(TeamCompChamp.TeamCompChampId.class)
 public class TeamCompChamp {
+
+    @Id
+    @Column(name = "temp_comp_id")
+    private Long teamCompId;
+
+    @Id
+    @Column(name = "champion_id")
+    private Long championId;
+
+    // Định nghĩa Composite Primary Key Class
+    @Getter
+    @Setter
+    @EqualsAndHashCode
+    public static class TeamCompChampId implements Serializable {
+        private Long teamCompId;
+        private Long championId;
+    }
 }
