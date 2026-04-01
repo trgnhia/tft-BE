d84c560472ce1d92031096647c29b706067df324
Nguyen Van Giap <giapnv@gem-corp.global>
2026-04-01 09:09:54 +0700

    add entity user for base

---
+++ src/main/java/org/example/common/entity/User.java
@@ -0,0 +1,21 @@
+package org.example.common.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.Instant;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+public class User extends AuditableEntity{
+    private String username;
+    private String email;
+    private String passwordHash;
+    private boolean enabled;
+    Instant lastLogoutAt;
+}
---
+++ src/main/java/org/example/common/enums/RoleName.java
@@ -0,0 +1,5 @@
+package org.example.common.enums;
+
+public enum RoleName {
+    USER,ADMIN,MANAGER
+}
