422025d4448db1f6feea8883c4084e1a60ff008a
Nguyen Van Giap <giapnv@gem-corp.global>
2026-04-01 10:47:42 +0700

    feat: add rbac entities
    # Conflicts:
    #	src/main/java/org/example/entities/User.java

---
+++ src/main/java/org/example/configuration/SecurityConfig.java
@@ -0,0 +1,7 @@
+package org.example.configuration;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SecurityConfig {
+}
---
+++ src/main/java/org/example/entities/Permission.java
@@ -0,0 +1,25 @@
+package org.example.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+import org.example.common.entity.AuditableEntity;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "permissions")
+@Getter
+@Setter
+public class Permission extends AuditableEntity {
+    @Column(name = "name")
+    private String name;
+    @Column(name = "description")
+    private String description;
+    @ManyToMany(mappedBy = "permissions")
+    private Set<Role> roles = new HashSet<>();
+}
