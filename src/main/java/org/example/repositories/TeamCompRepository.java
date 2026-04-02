a3a64fb1d76b6fe151465fa2b16ddf6dfdd4aad4
Hoanglv <hoanglv1106@gmail.com>
2026-04-02 10:50:29 +0700

    refactor:  TeamCompRepository

--- src/main/java/org/example/repositories/TeamCompRepository.java
+++ src/main/java/org/example/repositories/TeamCompRepository.java
@@ -1,4 +1,9 @@
 package org.example.repositories;
 
-public interface TeamCompRepository {
+import org.example.entities.TeamComp;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TeamCompRepository extends JpaRepository<TeamComp, Long> {
 }
