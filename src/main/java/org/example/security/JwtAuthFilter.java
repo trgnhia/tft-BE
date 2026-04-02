97c1a3bf85469d99c69a3aa1ea9fb3577f37d702
Nguyen Van Giap <giapnv@gem-corp.global>
2026-04-02 15:36:50 +0700

    feat: implement

--- src/main/java/org/example/security/JwtAuthFilter.java
+++ src/main/java/org/example/security/JwtAuthFilter.java
@@ -1,43 +1,63 @@
 package org.example.security;
 
+import io.jsonwebtoken.JwtException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import lombok.RequiredArgsConstructor;
 import org.example.util.JwtUtil;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.servlet.HandlerExceptionResolver;
 
 import java.io.IOException;
 import java.util.Arrays;
 
 @Component
-@RequiredArgsConstructor
 public class JwtAuthFilter extends OncePerRequestFilter {
     private final JwtUtil jwtUtil;
     private final UserDetailsService userDetailsService;
+    private final HandlerExceptionResolver exceptionResolver;
+
+    public JwtAuthFilter(JwtUtil jwtUtil, UserDetailsService userDetailsService, @Qualifier("handlerExceptionResolver") HandlerExceptionResolver exceptionResolver) {
+        this.jwtUtil = jwtUtil;
+        this.userDetailsService = userDetailsService;
+        this.exceptionResolver = exceptionResolver;
+    }
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         String token = extractTokenFromCookie(request);
-
-        if (token != null && jwtUtil.validateToken(token)) {
-            String username = jwtUtil.getUsernameFromToken(token);
-            SecurityUser user = (SecurityUser) userDetailsService.loadUserByUsername(username);
-
-            if (user.isEnabled() && jwtUtil.isIssuedAfterLogout(token, user.getLastLogoutAt())) {
-                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
-                authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-                SecurityContextHolder.getContext().setAuthentication(authToken);
+        try {
+            if (isValidToken(token)) {
+                authenticateUser(request, token);
             }
+        } catch (JwtException ex) {
+            SecurityContextHolder.clearContext();
+            exceptionResolver.resolveException(request, response, null, ex);
         }
         filterChain.doFilter(request, response);
+    }
+
+    private void authenticateUser(HttpServletRequest request, String token) {
+        String username = jwtUtil.getUsernameFromToken(token);
+        SecurityUser user = (SecurityUser) userDetailsService.loadUserByUsername(username);
+
+        if (user.isEnabled() && jwtUtil.isIssuedAfterLogout(token, user.getLastLogoutAt())) {
+            UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+            authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+            SecurityContextHolder.getContext().setAuthentication(authToken);
+        }
+    }
+
+    private boolean isValidToken(String token) {
+        return token != null && jwtUtil.validateToken(token);
     }
 
     private String extractTokenFromCookie(HttpServletRequest request) {
