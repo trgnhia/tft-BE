a4bb954be2b4ce29f17e01f995d000e50f76b88e
Hoanglv <hoanglv1106@gmail.com>
2026-04-02 14:12:14 +0700

    feat: create TeamCompController.java

--- src/main/java/org/example/controller/TeamCompController.java
+++ src/main/java/org/example/controller/TeamCompController.java
@@ -1,4 +1,11 @@
 package org.example.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/team-comp")
 public class TeamCompController {
 }
