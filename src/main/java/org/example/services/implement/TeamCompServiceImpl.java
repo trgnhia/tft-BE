84e5cd64804b6a54da7f823b4412d4423c3e8ba8
Hoanglv <hoanglv1106@gmail.com>
2026-04-02 14:11:08 +0700

    feat: create team comp mapper and update TeamCompServiceImpl.java

--- src/main/java/org/example/services/implement/TeamCompServiceImpl.java
+++ src/main/java/org/example/services/implement/TeamCompServiceImpl.java
@@ -1,24 +1,72 @@
 package org.example.services.implement;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.example.common.enums.ErrorCode;
+import org.example.common.exception.ResourceNotFoundException;
 import org.example.dto.teamcomp.TeamCompRequest;
 import org.example.dto.teamcomp.TeamCompResponse;
+import org.example.entities.Champ;
+import org.example.entities.TeamComp;
+import org.example.entities.TeamCompChamp;
+import org.example.mapper.TeamCompMapper;
+import org.example.repositories.ChampRepository;
+import org.example.repositories.TeamCompRepository;
 import org.example.services.TeamCompService;
+import org.example.util.MessageUtils;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class TeamCompServiceImpl implements TeamCompService {
+    private final TeamCompRepository teamCompRepository;
+    private final ChampRepository champRepository;
+    private final TeamCompMapper teamCompMapper;
+    @Override
+    @Transactional
+    public TeamCompResponse create(TeamCompRequest request) {
+
+        TeamComp teamComp = teamCompMapper.toEntity(request);
+
+        final TeamComp savedTeamComp = teamCompRepository.save(teamComp);
+
+        if (request.getChampionIds() != null && !request.getChampionIds().isEmpty()) {
+
+            List<Champ> foundChamps = champRepository.findAllById(request.getChampionIds());
+
+            List<TeamCompChamp> joinList = foundChamps.stream().map(champ -> {
+                TeamCompChamp tcc = new TeamCompChamp();
+                tcc.setTeamCompId(savedTeamComp.getId());
+                tcc.setChampionId(champ.getId());
+                tcc.setTeamComp(savedTeamComp);
+                tcc.setChamp(champ);
+                return tcc;
+            }).collect(Collectors.toList());
+
+            teamComp.setTeamCompChamps(joinList);
+
+            teamComp = teamCompRepository.save(teamComp);
+        }
+
+        return teamCompMapper.toResponse(teamComp);
+    }
 
     @Override
-    public TeamCompResponse create(TeamCompRequest request) {
-        return null;
+    public TeamCompResponse getById(Long id) {
+        TeamComp teamComp = teamCompRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException(
+                        MessageUtils.getMessage("error." + ErrorCode.NOT_FOUND.name(), "Team Comp (ID: " + id + ")")
+                ));
+
+        return teamCompMapper.toResponse(teamComp);
     }
 
     @Override
@@ -31,10 +79,7 @@ public class TeamCompServiceImpl implements TeamCompService {
 
     }
 
-    @Override
-    public TeamCompResponse getById(Long id) {
-        return null;
-    }
+
 
     @Override
     public Page<TeamCompResponse> filterTeamComps(Long setId, String keyword, List<String> styles, Long championId, Pageable pageable) {
