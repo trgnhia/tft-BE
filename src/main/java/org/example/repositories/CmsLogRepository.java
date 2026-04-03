5614698c6983b569428d5a5c84581eb6df790629
Hoanglv <hoanglv1106@gmail.com>
2026-04-03 09:31:09 +0700

    feat: add cmsRepo

--- src/main/java/org/example/repositories/CmsLogRepository.java
+++ src/main/java/org/example/repositories/CmsLogRepository.java
@@ -1,4 +1,8 @@
 package org.example.repositories;
 
-public interface CmsLogRepository {
+import org.example.entities.CmsLog;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CmsLogRepository  extends JpaRepository<CmsLog, Long> {
+
 }
