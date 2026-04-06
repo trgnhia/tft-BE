e484a882ed265576bc468e346f39cd2821f0458c
Hoanglv <hoanglv1106@gmail.com>
2026-04-06 10:02:41 +0700

    feat: add validation teamcomp

--- src/main/java/org/example/dto/teamcomp/TeamCompRequest.java
+++ src/main/java/org/example/dto/teamcomp/TeamCompRequest.java
@@ -4,27 +4,25 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
-
 import lombok.Setter;
-
 import java.util.List;
 
 @Getter
 @Setter
 public class TeamCompRequest {
 
-    @NotNull(message = "team.comp.setId.not_null")
+    @NotNull(message = "{team.comp.setId.not_null}")
     private Long setId;
 
-    @NotBlank(message = "team.comp.tier.not_blank")
+    @NotBlank(message = "{team.comp.tier.not_blank}")
     private String tier;
 
-    @NotBlank(message = "team.comp.name.not_blank")
+    @NotBlank(message = "{team.comp.name.not_blank}")
     private String name;
 
-    @NotBlank(message = "team.comp.style.not_blank")
+    @NotBlank(message = "{team.comp.style.not_blank}")
     private String style;
 
-    @NotEmpty(message = "team.comp.champIds.not_empty")
+    @NotEmpty(message = "{team.comp.champIds.not_empty}")
     private List<Long> championIds;
 }
