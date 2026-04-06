615e53394515134dcc18a153026f2ff32d5e0483
Hoanglv <hoanglv1106@gmail.com>
2026-04-06 10:27:13 +0700

    fix: validation for teamcomp

--- src/main/java/org/example/dto/teamcomp/TeamCompRequest.java
+++ src/main/java/org/example/dto/teamcomp/TeamCompRequest.java
@@ -3,6 +3,7 @@ package org.example.dto.teamcomp;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 import java.util.List;
@@ -15,14 +16,18 @@ public class TeamCompRequest {
     private Long setId;
 
     @NotBlank(message = "{team.comp.tier.not_blank}")
+    @Size(max = 10, message = "{team.comp.tier.size}")
     private String tier;
 
     @NotBlank(message = "{team.comp.name.not_blank}")
+    @Size(max = 255, message = "{team.comp.name.size}")
     private String name;
 
     @NotBlank(message = "{team.comp.style.not_blank}")
+    @Size(max = 255, message = "{team.comp.style.size}")
     private String style;
 
     @NotEmpty(message = "{team.comp.champIds.not_empty}")
+    @Size(max = 15, message = "{team.comp.champIds.size}")
     private List<Long> championIds;
 }
