60ea7fd883b77e076ae9bc389146459c3166a9cc
Nguyen Van Giap <giapnv@gem-corp.global>
2026-04-15 10:42:29 +0700

    feat: update role controller to match frontend and optimize code

--- src/main/java/org/example/dto/role/UpdateRolePermissionRequest.java
+++ src/main/java/org/example/dto/role/UpdateRolePermissionRequest.java
@@ -1,11 +1,10 @@
 package org.example.dto.role;
 
 import jakarta.validation.constraints.Size;
-import org.example.dto.user.PermissionDto;
 
 import java.util.Set;
 
 public record UpdateRolePermissionRequest(
         @Size(message = "{error.PERMISSION_MIN}")
-        Set<PermissionDto> permissions) {
+        Set<Long> permissionIds) {
 }
